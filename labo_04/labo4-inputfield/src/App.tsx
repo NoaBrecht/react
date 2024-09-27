bbf5524c4cac9ef3c3f8d1db3ca59fcb1730fe60
NoaBrecht <vandenberghenoa1@gmail.com>
2024-09-27 14:00:39 +0000

    Refactor input field component to render multiple input fields

--- labo_04/labo4-inputfield/src/App.tsx
+++ labo_04/labo4-inputfield/src/App.tsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
+
+
+
 const InputFields = () => {
-
   const [Value, setValue] = useState<string>("");
-
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setValue(event.target.value);
   }
-
   return (
-    <input type="text" name="" id="" value={Value} onChange={handleChange} />
-  );
+    <>
+      <input type="text" name="" id="" value={Value} onChange={handleChange} />
+      <input type="text" name="" id="" value={Value} onChange={handleChange} />
+      <input type="text" name="" id="" value={Value} onChange={handleChange} />
+      <input type="text" name="" id="" value={Value} onChange={handleChange} />
+      <input type="text" name="" id="" value={Value} onChange={handleChange} />
+    </>);
 }
 const App = () => {
   return (
     <>
-      <InputFields />
       <InputFields />
     </>
   );
