a619f0ab81a1f624fcd1988479758c65647da889
NoaBrecht <vandenberghenoa1@gmail.com>
2024-11-07 12:42:06 +0000

    textinput

--- labo_03/TextInput/src/App.tsx
+++ labo_03/TextInput/src/App.tsx
@@ -1,8 +1,13 @@
-
+import React from "react"
 
 const App = () => {
   return (
-    <></>
+    <div>
+    { Array.from({length: 10}).map((_, i) => {
+        return <input type="text" onChange={(e) => alert(`Textbox ${i} changed to ${e.target.value}`)}/>
+    })}
+  </div>
+
   )
 }
 
